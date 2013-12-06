50b9f0d8bc9a866c775a8147cc01a856f7dedaf2
Wolfgang Hobmaier <0woh0.android@gmail.com>
2013-12-06 15:02:14 +0100

    No more swa(g/p)!

--- configs/a2sd.mk
+++ configs/a2sd.mk
@@ -6,10 +6,8 @@ PRODUCT_COPY_FILES += \
 # Actually set up a2sd
 PRODUCT_COPY_FILES += \
     vendor/pa/prebuilt/a2sd/etc/init.d/05mountext:system/etc/init.d/05mountext \
-    vendor/pa/prebuilt/a2sd/etc/init.d/06handleswap:system/etc/init.d/06handleswap \
     vendor/pa/prebuilt/a2sd/etc/init.d/10apps2sd:system/etc/init.d/10apps2sd \
     vendor/pa/prebuilt/a2sd/etc/init.d/20extgapps:system/etc/init.d/20extgapps \
     vendor/pa/prebuilt/a2sd/bin/a2sd:system/bin/a2sd \
     vendor/pa/prebuilt/a2sd/bin/fix_permissions:system/bin/fix_permissions \
     vendor/pa/prebuilt/a2sd/xbin/zipalign:system/xbin/zipalign \
-    vendor/pa/prebuilt/a2sd/bin/enable_zram:system/bin/enable_zram
